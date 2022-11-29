ea498d547fdd4ee5d679c08522ff524383076d1d
BillStephens2022 <two4onebill@yahoo.com>
2022-11-29 16:08:40 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+// Questions array - an array of objects with the question, options, and correct answer.
+
+var questions = [
+    {
+        question: "What is JavaScript?",
+        options: 
+            [
+                "a) JavaScript is a scripting language used to make the website interactive", 
+                "b) JavaScript is an assembly language used to make the website interactive",
+                "c) JavaScript is a compiled language used to make the website interactive",
+                "d) None of the Above"
+            ],
+        answer: "a) JavaScript is a scripting language used to make the website interactive"
+    },
+    {
+        question: "Which of the following is correct about JavaScript?",
+        options: 
+            [
+                "a) JavaScript is an Object-Based language", 
+                "b) JavaScript is Assembly-language",
+                "c) JavaScript is an Object-Oriented language",
+                "d)  JavaScript is a High-level language"
+            ],
+        answer: "a) JavaScript is an Object-Based language"
+    },
+    {
+        question: "Which of the following languages can be used to build a website?",
+        options: 
+            [
+                "a) JavaScript", 
+                "b) HTML",
+                "c) CSS",
+                "d) All of the Above"
+            ],
+        answer: "d) All of the Above"
+    },
+    {
+        question: "What is the file called that contains much of the styling for a web page?",
+        options: 
+            [
+                "a) colorcode", 
+                "b) stylesheet",
+                "c) designsheet",
+                "d) stylefile"
+            ],
+        answer: "b) stylesheet"
+    },
+    {
+        question: "Where should you ideally place the <script> tag in your HTML file to link to your external JavaScript file?",
+        options: 
+            [
+                "a) anywhere within the <head> section", 
+                "b) anywhere you want, as long as it's placed somewhere within the <html> tags",
+                "c) at the bottom of the body element",
+                "d) In the <head> section, below the link to your CSS stylesheet"
+            ],
+        answer: "c) at the bottom of the body element"
+    },
+]
+
+var questionEl = document.getElementById("question");
+var optionsEl = document.getElementById("options")
+
+questionEl.textContent = "Question: " + questions[0].question;
+for (var i = 0; i < questions[i].options.length; i++) {
+    var answerButton = document.createElement("button");
+    optionsEl.appendChild(answerButton);
+    answerButton.innerHTML = questions[0].options[i];
+};
+
+
