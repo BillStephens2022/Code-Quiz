65dd13d73bee5be67f51eccb1b65c995840ce640
BillStephens2022 <two4onebill@yahoo.com>
2022-12-02 09:58:41 -0500

    fixed bug with sort function, added notes to highscores.js file and removed console.logs

--- assets/js/highscorescript.js
+++ assets/js/highscorescript.js
@@ -1,22 +1,16 @@
+/* retrieving items from local storage and sorting them in descending order.
+high scores are then displayed on highscore.html page */
 
 var highScoreList = document.getElementById("high-score-list");
 var storedHighScores = JSON.parse(localStorage.getItem("highScores"));
-console.log(storedHighScores);
-var sortedHighScores = storedHighScores.sort((a, b) => a.value - b.value);
-console.log(sortedHighScores);
-
-
+var sortedHighScores = storedHighScores.sort(( a, b ) => b.score - a.score);
 
 var liElement = document.createElement("li");
 highScoreList.appendChild(liElement);
 
-
-for (var i = 0; i < sortedHighScores.length; i++) {
-    console.log(sortedHighScores[i].userInitials + " " + sortedHighScores[i].score);
+for (var i = 0; i < sortedHighScores.length; i++) {   
     var liElement = document.createElement("li");
     highScoreList.appendChild(liElement);
     liElement.textContent = sortedHighScores[i].userInitials + " " + sortedHighScores[i].score;
     liElement.classList.add("scores-list");
-
-    
 };
