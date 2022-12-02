a54216daaf868602988ea5a02eae28cd393acb17
BillStephens2022 <two4onebill@yahoo.com>
2022-12-01 20:09:06 -0500

    separated questions array into separate javascript file - questions.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,4 @@
-// Questions array - an array of objects with the question, options, and correct answer.
-
-var questions = [
-    {
-        question: "What is JavaScript?",
-        options: 
-            [
-                "a) JavaScript is a scripting language used to make the website interactive", 
-                "b) JavaScript is an assembly language used to make the website interactive",
-                "c) JavaScript is a compiled language used to make the website interactive",
-                "d) None of the Above"
-            ],
-        answer: "a) JavaScript is a scripting language used to make the website interactive"
-    },
-    {
-        question: "Which of the following is correct about JavaScript?",
-        options: 
-            [
-                "a) JavaScript is an Object-Based language", 
-                "b) JavaScript is Assembly-language",
-                "c) JavaScript is an Object-Oriented language",
-                "d)  JavaScript is a High-level language"
-            ],
-        answer: "a) JavaScript is an Object-Based language"
-    },
-    {
-        question: "Which of the following languages can be used to build a website?",
-        options: 
-            [
-                "a) JavaScript", 
-                "b) HTML",
-                "c) CSS",
-                "d) All of the Above"
-            ],
-        answer: "d) All of the Above"
-    },
-    {
-        question: "What is the file called that contains much of the styling for a web page?",
-        options: 
-            [
-                "a) colorcode", 
-                "b) stylesheet",
-                "c) designsheet",
-                "d) stylefile"
-            ],
-        answer: "b) stylesheet"
-    },
-    {
-        question: "Where should you ideally place the <script> tag in your HTML file to link to your external JavaScript file?",
-        options: 
-            [
-                "a) anywhere within the <head> section", 
-                "b) anywhere you want, as long as it's placed somewhere within the <html> tags",
-                "c) at the bottom of the body element",
-                "d) In the <head> section, below the link to your CSS stylesheet"
-            ],
-        answer: "c) at the bottom of the body element"
-    },
-];
+// declaration of variables to capture various HTML elements.
 
 var questionEl = document.getElementById("question");
 var optionsEl = document.getElementById("options")
@@ -65,6 +7,7 @@ var startButton = document.getElementById("start-button");
 var commentEl = document.getElementById("comment");
 var initialsEl = document.getElementById("input-initials");
 
+// declaration of variables needed for game play.
 
 var timeLeft = 30;
 var questionsIndex = 0;
@@ -72,6 +15,10 @@ var score = 0;
 var gameOver;
 var userInitials = "";
 var highScores = [];
+
+
+// variables for audio objects which will be played when user answers
+// correctly (wooHoo!) and incorrectly (doh!)
 
 var doh = new Audio ("./assets/sounds/doh.mp3");
 var wooHoo = new Audio ("./assets/sounds/woohoo.mp3");
